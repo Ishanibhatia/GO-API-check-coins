d370897b17ef8c7c2e75265a15b9d0cd9d5d143b
Ishani <ishanibhatia2003@gmail.com>
2024-04-16 02:07:25 +0530

    adding git folder

---
+++ api/api.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+//coin balance parameter
+type CoinBalanceParams struct {
+	Username string
+}
+
+//Coin Balance Response
+type CoinBalanceResponse struct{
+	//Success code (usually 200)
+	Code int
+
+	//Account balance
+	Balance int64
+}
+
+//Error Response
+type Error strcut{
+	//Error code
+	Code int
+
+	//Error message 
+	Message string
+}
